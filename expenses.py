60a9ddaf922b210d95b64e85c73471ed25223ee8
LetsGoJoeUK <LetsGo@JoeUK.com>
2025-03-27 21:34:29 +0000

    added two more examples

---
+++ expenses.py
@@ -0,0 +1,16 @@
+
+
+
+expenses = [10.50,8,5,15,20,5,3]
+
+total = sum(expenses)
+print('You spent £',total,sep='')
+
+a = range(7)
+b = range(0,7,1)
+c = range(2,14,2)
+
+print(type(c))
+
+for i in range(7):
+    expenses.append(float(input("Enter your expenses : \n")))
---
+++ lists and loops.py
@@ -0,0 +1,10 @@
+acronyms = ['LOL','IDK','SMH','TBH']
+acronyms.append('IDK')
+acronyms.append('BFN')
+print(acronyms)
+word = 'BFN'
+
+if word in acronyms:
+    print(word+' is in the list')
+else:
+    print(word+' is not in the list')
